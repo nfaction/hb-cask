df8b3107f5f4ec7d9a19fed82f86a87b6e2f6f71
Matt DePorter <nfaction@gmail.com>
2015-02-09 15:32:33 -0700

    Reorganized installer to work better with pre-installer and did some cleanup

--- mac-brew-installer.sh
+++ mac-brew-installer.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
-xcode-select --install
-
 echo "Installing Homebrew..."
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "Install Cask -----------------------------"
-read -r -p "Are you sure? [y/N] " response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
-then
-    brew install caskroom/cask/brew-cask
-else
-	exit 1
-fi
 
 echo "Setting up 'brew bundle'..."
 brew tap homebrew/boneyard
 
-echo "export HOMEBREW_CASK_OPTS='--appdir=/Applications --caskroom=/etc/Caskroom'" >> ~/.bash_profile
+echo "Install Cask? -----------------------------"
+read -r -p "Are you sure? [y/N] " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    brew install caskroom/cask/brew-cask
+    #
+	# Change default Cask path to /Applications, not ~/Applications
+	#
+	echo "export HOMEBREW_CASK_OPTS='--appdir=/Applications --caskroom=/etc/Caskroom'" >> ~/.bash_profile
 
-echo "Setting up brew cask's permissions/directories..."
-MY_UID=`id -u`
-sudo chown $MY_UID /etc/Caskroom
+	#
+	# Set ownership of new Caskroom for user.  (This will thow errors in doctor and will not allow casks to be installed correctly)
+	#
+	echo "Setting up brew cask's permissions/directories..."
+	MY_UID=$(id -u)
+	sudo chown $MY_UID /etc/Caskroom
+else
+	echo "Not installing Cask..."
+	echo "Please run 'brew doctor' and resolve all issues manually"
+	exit 0
+fi
 
+#
+# Run Brew Doctor and resolve all issues
+#
 brew doctor
 
-echo "Please resolve warnings and errors expressed by 'brew doctor'.  Enter 'y' then 'Enter' if there are no warnings, otherwise, hit Control+C"
-read answer
+exit 0
 
+#
 ## Resources ##
+#
 # http://mattstauffer.co/blog/setting-up-a-new-os-x-development-machine-part-2-global-package-managers
 # https://github.com/caskroom/homebrew-cask/blob/master/USAGE.md
 # https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Installation.md
